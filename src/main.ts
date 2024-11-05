86887796b059626330af1df256bbd9b6d5751b8f
AnasIsmai1 <174011704+AnasIsmai1@users.noreply.github.com>
2024-11-06 01:08:06 +0500

    Feature: Added themes and toggle for themes
    Added css variables and a theme switcher on the header

--- src/main.ts
+++ src/main.ts
@@ -6,3 +6,9 @@ platformBrowserDynamic().bootstrapModule(AppModule, {
   ngZoneEventCoalescing: true
 })
   .catch(err => console.error(err));
+
+if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
+  document.documentElement.setAttribute('data-theme', 'dark');
+} else {
+  document.documentElement.setAttribute('data-theme', 'light');
+}
--- src/shared/ui/header/header.component.ts
+++ src/shared/ui/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, Output, EventEmitter } from '@angular/core';
+import { Component, Output, EventEmitter, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { MenuServiceService } from '../../../services/menu-service/menu-service.service';
@@ -11,16 +11,29 @@ import { MenuServiceService } from '../../../services/menu-service/menu-service.
   styleUrl: './header.component.scss'
 })
 
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
 
   constructor(private menuService: MenuServiceService) {
     this.menuService.booleanValue$.subscribe(value => this.isClicked = value);
   }
+  ngOnInit(): void {
+    this.isDarkTheme = document.documentElement.getAttribute('data-theme') == 'dark' ? true : false;
+  }
 
+  isDarkTheme!: boolean;
   isClicked: boolean = false;
   menuShow: boolean = false;
   logoShow: boolean = false;
   @Output() menuClick = new EventEmitter();
+
+  onThemeToggle() {
+    this.isDarkTheme = !this.isDarkTheme;
+    if (document.documentElement.getAttribute('data-theme') == 'dark') {
+      document.documentElement.setAttribute('data-theme', 'light')
+    } else {
+      document.documentElement.setAttribute('data-theme', 'dark')
+    }
+  }
 
   onElementHover(property: 'menuShow' | 'logoShow', isHovering: boolean) {
     if (screen.availWidth > 800) {
