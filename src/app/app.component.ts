690a1057175e3aae8582b63addade9a6c956d82c
AnasIsmai1 <174011704+AnasIsmai1@users.noreply.github.com>
2024-11-08 01:37:26 +0500

    Created a loader for initial and route navigations

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,41 @@
-import { Component } from '@angular/core';
+import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
 import { MenuServiceService } from '../services/menu-service/menu-service.service';
+import { NavigationCancel, NavigationEnd, NavigationStart, Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'portfolio';
+  loading: boolean = true;
+  isTrue: boolean = false;
 
   menuHidden: boolean = false;
-  constructor(private menuService: MenuServiceService) {
+  constructor(private menuService: MenuServiceService, private router: Router, private cdr: ChangeDetectorRef) {
     this.menuService.booleanValue$.subscribe(value => this.menuHidden = value);
+    this.loading = true;
   }
+
+  ngOnInit() {
+
+    this.router.events.subscribe((event) => {
+      if (event instanceof NavigationStart) {
+        this.loading = true;
+        console.log("Navigation Started");
+      } else if (event instanceof NavigationCancel || event instanceof NavigationEnd) {
+        setTimeout(() => {
+          this.loading = false;
+        }, 2000);
+        console.log("Navigation Ended or Cancelled");
+        console.log(this.loading);
+      }
+
+      this.cdr.detectChanges()
+    })
+
+  }
+
+
 }
