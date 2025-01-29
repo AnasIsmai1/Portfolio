0476640081cf340371bab2f5813e6daf8e08e859
AnasIsmai1 <174011704+AnasIsmai1@users.noreply.github.com>
2025-01-29 22:09:10 +0500

    Update: changed projects and adjusted stack icons

--- src/projects.ts
+++ src/projects.ts
@@ -7,14 +7,41 @@ export interface PROJECT {
   technologies: string[],
   isLoading: boolean,
 }
+
+const angular = "angular fa-angular";
+const js = "js fa-square-js";
+const scss = "scss fa-sass";
+const css = "css fa-css3-alt";
+const html = "html fa-html5";
+const react = "react fa-react";
+
 export const Projects: PROJECT[] = [
+
+  {
+    name: "GPA Calculator",
+    siteLink: "https://gpa-predictor.vercel.app/",
+    codeLink: "https://github.com/AnasIsmai1/gpa-predictor/",
+    details: "This GPA Calculator is a user-friendly web application built with React and Tailwind CSS. It features a clean interface where users can input their course information and instantly see their GPA calculation results visualized through an interactive line graph.",
+    img: "./gpa-calc.png",
+    technologies: [html, js, css, react],
+    isLoading: true,
+  },
+  {
+    name: "Get In Touch Chat App",
+    siteLink: "https://get-in-touch-beta.vercel.app/",
+    codeLink: "https://github.com/AnasIsmai1/ChatApp-Backend.git",
+    details: "This real-time chat application leverages WebSocket technology to enable instant messaging between users. Built with Node.js and Express on the backend, it uses Socket.io to manage WebSocket connections, allowing for seamless bidirectional communication. The frontend is crafted with React, featuring a clean and intuitive user interface.",
+    img: "./getintouch.png",
+    technologies: [html, js, css, react],
+    isLoading: true,
+  },
   {
     name: "Glint Landing Page",
     siteLink: "https://anas-sdeprj-glint-landing-page.netlify.app/",
     codeLink: "https://github.com/AnasIsmai1/Glint",
     details: "This project demonstrates API integration skills by using the Jikan API to fetch data from MyAnimeList. The webpage, built with HTML, CSS, and JavaScript, dynamically renders content cards, which are added to the DOM via document fragments for improved performance.",
     img: "./Glint.png",
-    technologies: ["scss", "html", "js", "css"],
+    technologies: [scss, html, js, css],
     isLoading: true,
   },
   {
@@ -23,25 +50,7 @@ export const Projects: PROJECT[] = [
     codeLink: "https://github.com/AnasIsmai1/popular-anime-data",
     details: "This project demonstrates API integration skills by using the Jikan API to fetch data from MyAnimeList. The webpage, built with HTML, CSS, and JavaScript, dynamically renders content cards, which are added to the DOM via document fragments for improved performance.",
     img: "./Anime.png",
-    technologies: ["scss", "html", "js", "css"],
-    isLoading: true,
-  }, {
-    name: 'Shades Landing Page',
-    siteLink: "https://anas-sdeprj-shades-landing-page.netlify.app/",
-    codeLink: "https://github.com/AnasIsmai1/Websites/tree/main/Shades-Landing-Page",
-    details: "This project is a news landing page. This was made using Angular, I made use of components to create the articles at the bottom and dynamically render them in.",
-    img: "./Shades.png",
-    technologies: ["html", "js", "css"],
-    isLoading: true,
-
-  },
-  {
-    name: 'News Homepage',
-    siteLink: "https://anas-sdeprj-news-homepage.vercel.app/",
-    codeLink: "https://github.com/AnasIsmai1/News-Homepage-Main",
-    details: "This project is a news landing page. This was made using Angular, I made use of components to create the articles at the bottom and dynamically render them in.",
-    img: "./News.png",
-    technologies: ["angular", "scss", "html", "js", "css"],
+    technologies: [scss, html, js, css],
     isLoading: true,
   }
 ]
